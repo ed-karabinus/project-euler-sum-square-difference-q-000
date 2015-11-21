702ab3604b16b4395782f792e4308c5025049315
ed-karabinus <ejkarabin@gmail.com>
2015-11-20 18:33:12 -0600

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,16 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize(parameter)
+    @parameter = parameter
+  end
+
+  def difference
+    sum = 0
+    sum_square = 0
+    (1..@parameter).each do |i|
+      sum += i
+      sum_square += i * i
+    end
+    sum * sum - sum_square
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+def sum_square_difference(number)
+  sum = 0
+  sum_square = 0
+  (1..number).each do |i|
+    sum += i
+    sum_square += i * i
+  end
+  sum * sum - sum_square
+end
